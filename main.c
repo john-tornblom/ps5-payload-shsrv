f94c676f19a5845aadc15576d73c885243bc9546
John Törnblom <john.tornblom@gmail.com>
2024-02-03 22:52:29 +0100

    cosmetics and life improvements

--- main.c
+++ main.c
@@ -16,15 +16,10 @@ along with this program; see the file COPYING. If not, see
 
 
 #include <arpa/inet.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <ifaddrs.h>
 #include <netinet/in.h>
-#include <pthread.h>
 #include <signal.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,83 +28,38 @@ along with this program; see the file COPYING. If not, see
 #include <sys/sysctl.h>
 #include <unistd.h>
 
-
 #include <ps5/kernel.h>
 
 
 #include "shell.h"
 
 
-/**
- * Data structure used to send UI notifications on the PS5.
- **/
-typedef struct notify_request {
-  char useless1[45];
-  char message[3075];
-} notify_request_t;
-
-
-int sceKernelSendNotificationRequest(int, notify_request_t*, size_t, int);
 int sceKernelSetProcessName(const char*);
-
-
-static void
-notify(const char *fmt, ...) {
-  notify_request_t req;
-  va_list args;
-
-  bzero(&req, sizeof req);
-  va_start(args, fmt);
-  vsnprintf(req.message, sizeof req.message, fmt, args);
-  va_end(args);
-
-  sceKernelSendNotificationRequest(0, &req, sizeof req, 0);
-  printf("%s\n", req.message);
-}
-
-
-static int
-init_client(int fd) {
-  for(int i=0; i<1024; i++) {
-    if(i != fd && fcntl(i, F_GETFD) > 0 && errno != EBADF) {
-      close(i);
-    }
-  }
-
-  if(dup2(fd, STDIN_FILENO) < 0) {
-    return -1;
-  }
-
-  if(dup2(fd, STDOUT_FILENO) < 0) {
-    return -1;
-  }
-
-  if(dup2(fd, STDERR_FILENO) < 0) {
-    return -1;
-  }
-
-  close(fd);
-  return 0;
-}
 
 
 /**
  * 
  **/
 static void
-spawn_client(int master, int slave) {
-  if (!syscall(SYS_rfork, RFPROC | RFNOWAIT | RFFDG)) {
-    close(master);
-    if(init_client(slave)) {
-      _exit(errno);
-    } else {
-      shell_loop();
-    }
-    _exit(0);
+spawn_shell(int srvfd, int fd) {
+  if(syscall(SYS_rfork, RFPROC | RFNOWAIT | RFFDG)) {
+    return;
   }
 
-  close(slave);
+  syscall(SYS_setsid);
+  sceKernelSetProcessName("sh");
+
+  dup2(fd, STDIN_FILENO);
+  dup2(fd, STDOUT_FILENO);
+  dup2(fd, STDERR_FILENO);
+
+  close(srvfd);
+  close(fd);
+
+  shell_loop();
+  _exit(0);
 }
+
 
 /**
  *
@@ -152,9 +102,8 @@ serve_shell(uint16_t port) {
     if(!strncmp("0.", ip, 2)) {
       continue;
     }
-
-    notify("Serving shell on %s:%d (%s)", ip, port, ifa->ifa_name);
     ifaddr_wait = 0;
+    printf("[shsrv.elf] Serving shell on %s:%d (%s)\n", ip, port, ifa->ifa_name);
   }
 
   freeifaddrs(ifaddr);
@@ -164,12 +113,12 @@ serve_shell(uint16_t port) {
   }
 
   if((srvfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
+    perror("[shsrv.elf] socket");
     return -1;
   }
 
   if(setsockopt(srvfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
-    perror("setsockopt");
+    perror("[shsrv.elf] setsockopt");
     return -1;
   }
 
@@ -179,24 +128,23 @@ serve_shell(uint16_t port) {
   server_addr.sin_port = htons(port);
 
   if(bind(srvfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
-    perror("bind");
+    perror("[shsrv.elf] bind");
     return -1;
   }
 
   if(listen(srvfd, 5) != 0) {
-    perror("listen");
+    perror("[shsrv.elf] listen");
     return -1;
   }
 
-  addr_len = sizeof(client_addr);
-
   while(1) {
+    addr_len = sizeof(client_addr);
     if((connfd=accept(srvfd, (struct sockaddr*)&client_addr, &addr_len)) < 0) {
-      perror("accept");
+      perror("[shsrv.elf] accept");
       break;
     }
-
-    spawn_client(srvfd, connfd);
+    spawn_shell(srvfd, connfd);
+    close(connfd);
   }
 
   return close(srvfd);
@@ -214,17 +162,17 @@ find_pid(const char* name) {
   uint8_t *buf;
 
   if(sysctl(mib, 4, 0, &buf_size, 0, 0)) {
-    perror("sysctl");
+    perror("[shsrv.elf] sysctl");
     return -1;
   }
 
   if(!(buf=malloc(buf_size))) {
-    perror("malloc");
+    perror("[shsrv.elf] malloc");
     return -1;
   }
 
   if(sysctl(mib, 4, buf, &buf_size, 0, 0)) {
-    perror("sysctl");
+    perror("[shsrv.elf] sysctl");
     return -1;
   }
 
@@ -250,17 +198,22 @@ main(void) {
   const int port = 2323;
   pid_t pid;
 
+  signal(SIGCHLD, SIG_IGN);
   if(syscall(SYS_rfork, RFPROC | RFNOWAIT | RFFDG)) {
     return 0;
   }
-  syscall(SYS_setsid); 
 
+  signal(SIGCHLD, SIG_IGN);
   if((pid=find_pid("shsrv.elf")) > 0) {
     kill(pid, SIGKILL);
     sleep(1);
   }
 
-  sceKernelSetProcessName("shsrv.elf");
+  syscall(SYS_setsid);
+  syscall(SYS_thr_set_name, -1, "shsrv.elf");
+  dup2(open("/dev/console", O_WRONLY), STDOUT_FILENO);
+  dup2(open("/dev/console", O_WRONLY), STDERR_FILENO);
+
   while(1) {
     serve_shell(port);
     sleep(3);
